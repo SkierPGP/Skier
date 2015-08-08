bb45161e5095b4c25696a8999764a075f3c68853
SunDwarf <eyesismine@gmail.com>
2015-08-08 12:00:48 +0100

    Update some templates

--- skier.py
+++ skier.py
@@ -1,7 +1,12 @@
+from flask import render_template
 from app import app
 
 import cfg
 
+
+@app.route("/")
+def index():
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run()
