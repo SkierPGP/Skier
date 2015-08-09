ce05334584907313ea8546a7d18cab264f3c7da7
SunDwarf <eyesismine@gmail.com>
2015-08-09 22:44:23 +0100

    Update config files

--- cfg.py
+++ cfg.py
@@ -2,5 +2,7 @@ import configmaster
 
 cfg = configmaster.YAMLConfigFile.YAMLConfigFile("config.yml")
 
+cfg.apply_defaults(configmaster.YAMLConfigFile.YAMLConfigFile("config.default.yml"))
+
 API_VERSION = 1
 SKIER_VERSION = 1.0
