09c929d13d26bd2f23e5dbf7cc93d01a1a95dca7
SunDwarf <eyesismine@gmail.com>
2015-08-10 14:04:33 +0100

    Add new links and frontend route.

--- skier/frontend.py
+++ skier/frontend.py
@@ -1,15 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, request
 
 frontend = Blueprint("frontend", __name__)
 
 @frontend.route("/about")
 def about():
     return render_template("about.html",
-        add_link="javascript:$(\"#addDialogue\").slideDown();",
-        search_link="")
+        add_link=url_for("frontend.add"),
+        search_link=url_for("frontend.search"))
         #currkeys=len(gpg.list_keys())) # This is commented out right now because I can't think of a fast way to implement it.
                                         # If you can think of a better way to do this than loading all keys into memory, go ahead.
+@frontend.route("/add")
+def add():
+    return "", 200
 
 @frontend.route("/search")
 def search():
-    return render_template("search.html")
+
+    if 'keyid' in request.args:
+        return render_template("search.html", search=request.args.get('keyid'))
+    else:
+        return render_template("search.html")
