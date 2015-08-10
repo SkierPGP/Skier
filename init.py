c1e2f79db3af0ec676d6ddac8e93b3139d33de61
SunDwarf <eyesismine@gmail.com>
2015-08-10 20:19:11 +0100

    Add custom error pages.

--- init.py
+++ init.py
@@ -1,4 +1,4 @@
-from flask import url_for
+from flask import url_for, render_template
 from werkzeug.utils import redirect
 
 
@@ -11,6 +11,14 @@ def init(app):
     app.register_blueprint(frontend.frontend_keys, url_prefix="/keys")
     app.register_blueprint(pgpapi.pgpapi, url_prefix="/api/v{}".format(API_VERSION))
     app.register_blueprint(pks.legacypks, url_prefix="/pks")
+
+    @app.errorhandler(404)
+    def four_oh_four(error):
+        return render_template("error/404.html"), 404
+
+    @app.errorhandler(403)
+    def four_oh_three(error):
+        return render_template("error/403.html"), 403
 
     @app.route("/")
     def index():
