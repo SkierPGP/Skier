24a0924230640d7aa34e461ff8cfd5bebcc02436
SunDwarf <eyesismine@gmail.com>
2015-08-10 14:58:59 +0100

    Add working search interface

--- init.py
+++ init.py
@@ -8,6 +8,7 @@ def init(app):
     from skier import pks
     from cfg import API_VERSION
     app.register_blueprint(frontend.frontend)
+    app.register_blueprint(frontend.frontend_keys, url_prefix="/keys")
     app.register_blueprint(pgpapi.pgpapi, url_prefix="/api/v{}".format(API_VERSION))
     app.register_blueprint(pks.legacypks, url_prefix="/pks")
 
--- skier/frontend.py
+++ skier/frontend.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template, url_for, request
 
+from skier import pgp
+
 frontend = Blueprint("frontend", __name__)
+frontend_keys = Blueprint("user_display_keys", __name__)
 
 @frontend.route("/about")
 def about():
@@ -13,10 +16,28 @@ def about():
 def add():
     return "", 200
 
+
 @frontend.route("/search")
 def search():
-
     if 'keyid' in request.args:
-        return render_template("search.html", search=request.args.get('keyid'))
+        # Pass a simple list of KeyInfos to the renderer, allowing the template to do the hard work.
+        keys = pgp.search_through_keys(request.args.get("keyid", "0x12345678"))
+        return render_template("search.html", search=request.args.get('keyid'), keys=keys)
     else:
         return render_template("search.html")
+
+@frontend_keys.route("/<keyid>")
+def getkey(keyid):
+    key = pgp.get_pgp_armor_key(keyid)
+    if key:
+        return key, 200, {"Content-Type": "text/plain"}
+    else:
+        return "No such key", 404, {"Content-Type": "text/plain"}
+
+@frontend_keys.route("/<keyid>/dl")
+def getkey_raw(keyid):
+    key = pgp.get_pgp_armor_key(keyid)
+    if key:
+        return key, 200, {"Content-Type": "text/plain", "Content-Disposition": "attachment; filename={}.asc".format(keyid)}
+    else:
+        return "No such key", 404, {"Content-Type": "text/plain"}
