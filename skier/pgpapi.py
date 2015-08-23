2d8dafe83e875c76f8e24237e99637ce669f05ee
SunDwarf <eyesismine@gmail.com>
2015-08-23 13:11:53 +0100

    Update PGPAPI to disable the raw route.

--- skier/pgpapi.py
+++ skier/pgpapi.py
@@ -20,14 +20,18 @@ def getkey(keyid):
         return json.dumps({"key": None}), 404, {"Content-Type": "application/json",
                                                 "Cache-Control": "no-cache", "Pragma": "no-cache"}
 
-@pgpapi.route("/getkey/<keyid>/raw")
-def getkey_raw(keyid):
-    # Load the key.
-    key = pgp.get_pgp_key(keyid)
-    if key:
-        return key, 200, {"Content-Type": "application/octet-stream",
-                          "Content-Disposition": "attachment; filename=gpgkey.asc",
-                          "Cache-Control": "no-cache", "Pragma": "no-cache"}
+# Disabled for now due to we can't get the key raw.
+#@pgpapi.route("/getkey/<keyid>/raw")
+#def getkey_raw(keyid):
+#    # Load the key.
+#    key = pgp.get_pgp_key(keyid)
+#    if key:
+#        return key, 200, {"Content-Type": "application/octet-stream",
+#                          "Content-Disposition": "attachment; filename=gpgkey.asc",
+#                          "Cache-Control": "no-cache", "Pragma": "no-cache"}
+
+
+
 
 @pgpapi.route("/addkey")
 def addkey():
