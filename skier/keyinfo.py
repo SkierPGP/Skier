SkierPGP/Skier

commit 2f6f5accae255d43af885c12e3582821d5023172

Panic and cry if we can't find a valid pubkey packet.

skier/keyinfo.py
@@ -347,6 +347,10 @@ class KeyInfo(object):
             most_recent_packet = packet
             npackets.append(packet)
 
+        if not pgpdump.packet.PublicKeyPacket in npackets:
+            # Panic and cry.
+            return None
+
         if cfg.cfg.config.features.armor_rewrite:
             # Re-construct the armored data.
             s = b''.join([p.original_data for p in npackets])

skier/frontend.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template, url_for, request, redirect
-
 import db
 from skier import pgp
 from cfg import cfg
@@ -9,27 +8,33 @@ from skier.pgpactions import broadcast_add
 frontend = Blueprint("frontend", __name__)
 frontend_keys = Blueprint("user_display_keys", __name__)
 
+
 @frontend.route("/")
 def index():
     return render_template("index.html", host=cfg.config.hostname, keys_served=db.Key.query.count())
 
+
 @frontend.route("/about/pool")
 def pool():
     return render_template("about/pool.html",
-            autosynch=cfg.config.pool_enabled.autosync, autoupload=cfg.config.pool_enabled.syncactions,
-            pool_servers=cfg.config.keyservers_synch, poolname=cfg.config.pool_name)
+                           autosynch=cfg.config.pool_enabled.autosync, autoupload=cfg.config.pool_enabled.syncactions,
+                           pool_servers=cfg.config.keyservers_synch, poolname=cfg.config.pool_name)
+
 
 @frontend.route("/about/faq")
 def faq():
     return render_template("about/faq.html")
 
+
 @frontend.route("/about/pgp")
 def whatispgp():
     return render_template("about/pgp.html")
 
+
 @frontend.route("/about/skier-vs-sks")
 def skier_vs_sks():
     return render_template("about/skier_vs_sks.html")
+
 
 @frontend.route("/add", methods=["GET", "POST"])
 def add():
@@ -42,16 +47,19 @@ def add():
         imported = pgp.add_pgp_key(key)
         if not imported[0]:
             if imported[1] == -1:
-                return render_template("submit.html", success=False, errormsg="Seems like an error happened importing your key. Double-check you copy/pasted it correctly.")
+                return render_template("submit.html", success=False,
+                                       errormsg="Seems like an error happened importing your key. Double-check you copy/pasted it correctly.")
             elif imported[1] == -2:
-                return render_template("submit.html", success=False, errormsg="Your key is already added on the server and is unchanged.")
+                return render_template("submit.html", success=False,
+                                       errormsg="Your key is already added on the server and is unchanged.")
             elif imported[1] == -3:
-                return render_template("submit.html", success=False, errormsg="Your key was invalid and could not be imported.")
+                return render_template("submit.html", success=False,
+                                       errormsg="Your key was invalid and could not be imported.")
         else:
             keyinfo = pgp.get_pgp_keyinfo(imported[1])
             broadcast_add(imported[2])
             return redirect(url_for("frontend.getkeyinfo", key=keyinfo.shortid, added=True)), 302
-            #return render_template("keyinfo.html", added=True, key=keyinfo, keydata=key)
+            # return render_template("keyinfo.html", added=True, key=keyinfo, keydata=key)
 
 
 @frontend.route("/keyinfo/<key>", methods=["GET", "POST"])
@@ -60,8 +68,10 @@ def getkeyinfo(key: str):
         key = key.replace("0x", "")
     # Keyinfo route.
     keydata = pgp.get_pgp_keyinfo(key)
-    if request.args.get("added", False): added = True
-    else: added = False
+    if request.args.get("added", False):
+        added = True
+    else:
+        added = False
     if keydata:
         keyascii = pgp.get_pgp_armor_key(key)
         return render_template("keyinfo.html", added=added, key=keydata, keydata=keyascii, found=True)
@@ -86,6 +96,7 @@ def search():
     else:
         return render_template("search.html")
 
+
 @frontend.route("/import")
 def import_key():
     return render_template("import.html", importing=cfg.config.features.importing), 200
@@ -104,14 +115,7 @@ def getkey(keyid):
 def getkey_dl_ascii(keyid):
     key = pgp.get_pgp_armor_key(keyid)
     if key:
-        return key, 200, {"Content-Type": "text/plain", "Content-Disposition": "attachment; filename={}.asc".format(keyid)}
+        return key, 200, {"Content-Type": "text/plain",
+                          "Content-Disposition": "attachment; filename={}.asc".format(keyid)}
     else:
         return "No such key", 404, {"Content-Type": "text/plain"}
-
-#@frontend_keys.route("/<keyid>/dl/raw")
-#def getkey_dl_raw(keyid):
-#    key = pgp.get_pgp_armor_key(keyid)
-#    if key:
-#        return key, 200, {"Content-Type": "application/octet-stream", "Content-Disposition": "attachment; filename={}.gpg".format(keyid)}
-#    else:
-#        return "No such key", 404, {"Content-Type": "text/plain"}

commit 1ee9af44aa09f6bc64847ded2fc8d377257bd90e

fix bug introduced by 2f6f5accae255d43af885c12e3582821d5023172 plus fix error handling problems

.gitignore
@@ -4,6 +4,7 @@
 __pycache__/
 *.py[cod]
 *$py.class
+*.sw[op]
 
 # C extensions
 *.so
@@ -69,5 +70,3 @@ config.yml
 
 # Ignore PyCharm files
 .idea/
-
-

skier/keyinfo.py
@@ -280,7 +280,10 @@ class KeyInfo(object):
                     break
                 # Set data
                 created = packet.creation_time.replace(tzinfo=datetime.timezone.utc).timestamp()
-                expires = packet.expiration_time.replace(tzinfo=datetime.timezone.utc).timestamp() if packet.expiration_time is not None else None if not expires else expires
+                expires = (
+                    packet.expiration_time.replace(tzinfo=datetime.timezone.utc).timestamp()
+                    if packet.expiration_time is not None else None if not expires else expires
+                )
 
                 try:
                     algo = PGPAlgo(packet.raw_pub_algorithm)
@@ -347,8 +350,10 @@ class KeyInfo(object):
             most_recent_packet = packet
             npackets.append(packet)
 
-        if not pgpdump.packet.PublicKeyPacket in npackets:
-            # Panic and cry.
+        for packet in npackets:
+            if isinstance(packet, pgpdump.packet.PublicKeyPacket):
+                break
+        else:  # Panic and cry.
             return None
 
         if cfg.cfg.config.features.armor_rewrite:

skier/pgp.py
@@ -29,7 +29,7 @@ def add_pgp_key(armored: str) -> tuple:
     # Dump the key data.
     newkey = keyinfo.KeyInfo.pgp_dump(armored)
     # You tried, pgpdump. And that's more than we could ever ask of you.
-    if not newkey:
+    if not isinstance(newkey, keyinfo.KeyInfo):
         return False, -1, None
 
     # Put the data into the database.

skier/pgpapi.py
@@ -61,9 +61,9 @@ def get_pgpapi_keyinfo(keyid):
 
 @pgpapi.route("/addkey", methods=["POST", "PUT"])
 def addkey():
-    # Read in the key data from the form.
+    # Read in the key data from the url arguments. Goes like ?keydata=.
     try:
-        keydata = request.args.get("keydata")
+        keydata = request.args["keydata"]
     except KeyError:
         return json.dumps({"error": 1, "msg": "no-key"}), 401, {"Content-Type": "application/json"}
     # Attempt to add the key.